2aff1086319d0f1d06d498294588c265e97a5155
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-07-23 15:26:32 +0300

    feat: response import fix

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,5 +1,5 @@
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, ImageFocusedOutputs, \
+from components.BlurDetection.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, ImageFocusedOutputs, \
     ImageFocusedResponse, ImageFocusedExecutor, DetectionFocusedOutputs, DetectionFocusedResponse, DetectionFocusedExecutor, OutputImage
 
 
