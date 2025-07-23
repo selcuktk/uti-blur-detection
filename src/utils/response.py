fb6f15091b194ee8b3330089e4c852403eb69187
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-07-23 15:19:56 +0300

    feat: response.py responsed functions of executors added

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,14 +1,26 @@
-
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
+from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, ImageFocusedOutputs, \
+    ImageFocusedResponse, ImageFocusedExecutor, DetectionFocusedOutputs, DetectionFocusedResponse, DetectionFocusedExecutor, OutputImage
 
 
-def build_response(context):
+def build_response_imageFocused(context):
     outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
+    Outputs = ImageFocusedOutputs(outputImage=outputImage)
+    imageFocusedResponse = ImageFocusedResponse(outputs=Outputs)
+    imageFocusedExecutor = ImageFocusedExecutor(value=imageFocusedResponse)
+    executor = ConfigExecutor(value=imageFocusedExecutor)
+    packageConfigs = PackageConfigs(executor=executor)
+    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
+    packageModel = package.build_model(context)
+    return packageModel
+
+
+def build_response_detectionFocused(context):
+    outputImage = OutputImage(value=context.image)
+    Outputs = DetectionFocusedOutputs(outputImage=outputImage)
+    detectionFocusedResponse = DetectionFocusedResponse(outputs=Outputs)
+    detectionFocusedExecutor = DetectionFocusedExecutor(value=detectionFocusedResponse)
+    executor = ConfigExecutor(value=detectionFocusedExecutor)
     packageConfigs = PackageConfigs(executor=executor)
     package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
     packageModel = package.build_model(context)
