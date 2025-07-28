de2e0200edc14f0e2fcb3cea7987cf1d207c4234
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-07-28 11:25:09 +0300

    feat: DetectionFocused executor structural change

--- src/executors/DetectionFocused.py
+++ src/executors/DetectionFocused.py
@@ -1,7 +1,6 @@
 import os
 import cv2
 import sys
-import math
 import numpy as np
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
@@ -41,15 +40,31 @@ class DetectionFocused(Component):
     def bootstrap(config: dict) -> dict:
         return {}
 
+    def blurring_gaussian(self, image):
+        return cv2.GaussianBlur(image, (self.kernel_size, self.kernel_size), 0)
+
+    def blurring_average(self, image):
+        return cv2.blur(image, (self.kernel_size, self.kernel_size))
+
+    def blurring_median(self, image):
+        # medianBlur does not support given type, firstly image translated into uint8, secondly medianBlur function called, finally image is turned into back format
+        # part of information is lost in this transformation. But it is not critical since it is an image processing operation rather than being deep learning training data
+        image_uint8 = image.astype(np.uint8)
+        blurred_uint8 = cv2.medianBlur(image_uint8, self.kernel_size)
+        return blurred_uint8.astype(np.float32)
+
+    def blurring_bilateral(self, image):
+        return cv2.bilateralFilter(image, self.kernel_size, 75, 75)
+
     def blurring(self, image):
         if self.blur_type == "BlurGaussian":
-            blurred_image = cv2.GaussianBlur(image, (self.kernel_size, self.kernel_size), 0)
+            blurred_image = self.blurring_gaussian(image)
         elif self.blur_type == "BlurAverage":
-            blurred_image = cv2.blur(image, (self.kernel_size, self.kernel_size))
+            blurred_image = self.blurring_average(image)
         elif self.blur_type == "BlurMedian":
-            blurred_image = cv2.medianBlur(image, self.kernel_size)
+            blurred_image = self.blurring_median(image)
         elif self.blur_type == "BlurBilateral":
-            blurred_image = cv2.bilateralFilter(image, self.kernel_size, 75, 75)
+            blurred_image = self.blurring_bilateral(image)
         else:
             raise ValueError(f"Unknown blur type: {self.blur_type}")
 
