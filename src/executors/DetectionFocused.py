c287ed7b70b8d9577bc6adc8e8fa45ca5ab5dba9
selcuktk <157029095+selcuktk@users.noreply.github.com>
2025-07-28 14:18:20 +0300

    feat: DetectionFocused executor detections integrated into blurring function

--- src/executors/DetectionFocused.py
+++ src/executors/DetectionFocused.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import sys
+import math
 import numpy as np
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../../../'))
@@ -56,23 +57,42 @@ class DetectionFocused(Component):
     def blurring_bilateral(self, image):
         return cv2.bilateralFilter(image, self.kernel_size, 75, 75)
 
-    def blurring(self, image):
-        if self.blur_type == "BlurGaussian":
-            blurred_image = self.blurring_gaussian(image)
-        elif self.blur_type == "BlurAverage":
-            blurred_image = self.blurring_average(image)
-        elif self.blur_type == "BlurMedian":
-            blurred_image = self.blurring_median(image)
-        elif self.blur_type == "BlurBilateral":
-            blurred_image = self.blurring_bilateral(image)
-        else:
-            raise ValueError(f"Unknown blur type: {self.blur_type}")
+    def blurring_detections(self, image):
+        blurred_image = image.copy()
+
+        for det in self.detections:
+            bbox = det.get('boundingBox', {})
+            top = int(bbox.get('top', 0))
+            left = int(bbox.get('left', 0))
+            height = int(bbox.get('height', 0))
+            width = int(bbox.get('width', 0))
+
+            y1, y2 = top, top + height
+            x1, x2 = left, left + width
+
+            y1, y2 = max(0, y1), min(blurred_image.shape[0], y2)
+            x1, x2 = max(0, x1), min(blurred_image.shape[1], x2)
+
+            roi = blurred_image[y1:y2, x1:x2]
+
+            if self.blur_type == "BlurGaussian":
+                blurred_roi = self.blurring_gaussian(roi)
+            elif self.blur_type == "BlurAverage":
+                blurred_roi = self.blurring_average(roi)
+            elif self.blur_type == "BlurMedian":
+                blurred_roi = self.blurring_median(roi)
+            elif self.blur_type == "BlurBilateral":
+                blurred_roi = self.blurring_bilateral(roi)
+            else:
+                raise ValueError(f"Unknown blur type: {self.blur_type}")
+
+            blurred_image[y1:y2, x1:x2] = blurred_roi
 
         return blurred_image
 
     def run(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
-        img.value = self.blurring(img.value)
+        img.value = self.blurring_detections(img.value)
         self.image = Image.set_frame(img=img, package_uID=self.request.model.uID, redis_db=self.redis_db)
         packageModel = build_response_detectionFocused(context=self)
         return packageModel
